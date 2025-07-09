06f90bcb7c9d952d6b775f006f7e9a5c67d95bd6
Arnoy Goswami <arnoy799@gmail.com>
2025-07-09 12:49:28 +0000

    I dont care about expose . just change the api in the rolled back versio

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: 'AIzaSyApUm-BZdLiYhNoy1lfqZ0xwCB2Vw-kbts',
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
